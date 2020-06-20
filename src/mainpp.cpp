8f4916491b9bb6596395165e8890a1e147494bbc
jacob <jacob.hernandezsanchez@epfl.ch>
2020-06-20 13:32:13 +0200

    can do platform.step()

--- src/mainpp.cpp
+++ src/mainpp.cpp
@@ -7,6 +7,10 @@
 #include "definitions.h"
 
 Platform platform;
+Ticker doCommFlipper;
+Thread th_Control;
+
+volatile bool flag_doComm;
 
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart){
   platform._nh.getHardware()->flush();
@@ -16,14 +20,33 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
   platform._nh.getHardware()->reset_rbuf();
 }
 
+void doCommCb()
+{
+  flag_doComm=true;
+}
+
+void doControlTH() {
+  while (!platform._stop) {
+    platform.step();
+  }
+}
+
 void setup(void)
 {
   platform.init();
+  flag_doComm=false;
+
+  doCommFlipper.attach_us(&doCommCb,COMM_LOOP);
+  th_Control.start(doControlTH);
 }
 
 void loop(void)
 {
-  platform.communicateToRos();
-  DWT_Delay_us(2000);
+  if (flag_doComm)
+  {
+    platform.communicateToRos();
+    flag_doComm=false;
+  }
+
 }
 
